4be3642092d185635fb3e7dc918e955de8def3e0
Bowen Liu <reouct@gmail.com>
2024-10-31 16:02:04 -0600

    Update

--- 08-lab-socket-hunt/treasure_hunter.c
+++ 08-lab-socket-hunt/treasure_hunter.c
@@ -1,7 +1,3 @@
-// Replace PUT_USERID_HERE with your actual BYU CS user id, which you can find
-// by running `id -u` on a CS lab machine.
-#define USERID 1823730042
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -9,7 +5,9 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
-#include <unistd.h>
+#include <unistd.h>  // Include this header for close()
+//1823730042
+#define USERID 1823730042
 
 #include "sockhelper.h"
 
@@ -18,11 +16,12 @@ int verbose = 0;
 void print_bytes(unsigned char *bytes, int byteslen);
 
 int main(int argc, char *argv[]) {
-    if (argc != 5) {
+        if (argc != 5) {
         fprintf(stderr, "Usage: %s server port level seed\n", argv[0]);
         return 1;
     }
 
+    // Command-Line Arguments
     char *server = argv[1];
     char *port_str = argv[2];
     char *level_str = argv[3];
@@ -36,72 +35,152 @@ int main(int argc, char *argv[]) {
     printf("Port: %d\n", port);
     printf("Level: %d\n", level);
     printf("Seed: %d\n", seed);
+    // Check point 1
 
-	// Initial request
-    unsigned char message[8];
+    // Initial Request
+    unsigned char message[8] = {0};
     message[0] = 0;
     message[1] = (unsigned char)level;
 
-    unsigned int userid_n = htonl(USERID);
-    memcpy(&message[2], &userid_n, sizeof(userid_n));
+    unsigned int userid_nbo = htonl(USERID);
+    memcpy(&message[2], &userid_nbo, sizeof(userid_nbo));
 
-    unsigned short seed_n = htons((unsigned short)seed);
-    memcpy(&message[6], &seed_n, sizeof(seed_n));
+    unsigned short seed_nbo = htons((unsigned short)seed);
+    memcpy(&message[6], &seed_nbo, sizeof(seed_nbo));
 
+    // Print the message buffer
     print_bytes(message, 8);
-
 	// Check point 2 
-	struct addrinfo hints, *res, *rp;
-    int sockfd;
+
+	// Convert port to string
     char port_str_conv[6];
+    sprintf(port_str_conv, "%d", port);
 
-	sprintf(port_str_conv, "%d", port);
-
+    // Get server address information
+    struct addrinfo hints, *res, *rp;
     memset(&hints, 0, sizeof(hints));
-    hints.ai_family = AF_INET;
-    hints.ai_socktype = SOCK_DGRAM;
+    hints.ai_family = AF_INET;  // IPv4
+    hints.ai_socktype = SOCK_DGRAM;  // UDP
 
-    if (getaddrinfo(server, port_str_conv, &hints, &res) != 0) {
-        perror("getaddrinfo");
+    int s = getaddrinfo(server, port_str_conv, &hints, &res);
+    if (s != 0) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(s));
         return 1;
     }
 
-    for (rp = res; rp != NULL; rp = rp->ai_next) {
-        sockfd = socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol);
-        if (sockfd == -1) continue;
-
-        struct sockaddr_storage remote_addr_ss;
-        struct sockaddr *remote_addr = (struct sockaddr *)&remote_addr_ss;
-        memcpy(remote_addr, rp->ai_addr, sizeof(struct sockaddr_storage));
-
-        if (sendto(sockfd, message, sizeof(message), 0, remote_addr, rp->ai_addrlen) == -1) {
-            perror("sendto");
-            close(sockfd);
-            continue;
-        }
-
-        unsigned char response[256];
-        socklen_t addr_len = sizeof(struct sockaddr_storage);
-        int response_len = recvfrom(sockfd, response, sizeof(response), 0, remote_addr, &addr_len);
-        if (response_len == -1) {
-            perror("recvfrom");
-            close(sockfd);
-            continue;
-        }
-
-        printf("Received %d bytes from server\n", response_len);
-        print_bytes(response, response_len);
-
-        close(sockfd);
-        break;
+    // Create socket
+    int sock = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sock == -1) {
+        perror("socket");
+        return 1;
     }
 
-	
+    // Send message to server
+    struct sockaddr_storage remote_addr_ss;
+    struct sockaddr *remote_addr = (struct sockaddr *)&remote_addr_ss;
+
+    for (rp = res; rp != NULL; rp = rp->ai_next) {
+        memcpy(remote_addr, rp->ai_addr, sizeof(struct sockaddr_storage));
+        if (sendto(sock, message, sizeof(message), 0, remote_addr, rp->ai_addrlen) == -1) {
+            perror("sendto");
+            continue;
+        }
+        break;  // Successfully sent
+    }
+
+    if (rp == NULL) {
+        fprintf(stderr, "Could not send message\n");
+        return 1;
+    }
+
+    // Receive response from server
+    unsigned char response[256];
+    socklen_t addr_len = sizeof(struct sockaddr_storage);
+    ssize_t response_len = recvfrom(sock, response, sizeof(response), 0, remote_addr, &addr_len);
+    if (response_len == -1) {
+        perror("recvfrom");
+        return 1;
+    }
+
+    // Print the size of the response and its contents
+    printf("Received %zd bytes from server\n", response_len);
+    print_bytes(response, response_len);
 
     freeaddrinfo(res);
+    close(sock);
+	// Checkpoint 3
+
+    // Extract chunk length
+    unsigned char chunklen = response[0];
+
+    if (chunklen == 0) {
+        printf("Hunt is over.\n");
+        return 0;
+    } else if (chunklen > 127) {
+        printf("Error detected: %x\n", chunklen);
+        return 1;
+    }
+
+    // Extract treasure chunk
+    char chunk[128];
+    memcpy(chunk, &response[1], chunklen);
+    chunk[chunklen] = '\0';  // Null-terminate the chunk
+
+    // Extract op-code
+    unsigned char opcode = response[chunklen + 1];
+
+    // Extract op-param
+    unsigned short opparam;
+    memcpy(&opparam, &response[chunklen + 2], sizeof(opparam));
+    opparam = ntohs(opparam);  // Convert from network byte order to host byte order
+
+    // Extract nonce
+    unsigned int nonce;
+    memcpy(&nonce, &response[chunklen + 4], sizeof(nonce));
+    nonce = ntohl(nonce);  // Convert from network byte order to host byte order
+
+    // Print extracted values
+    printf("%x\n", chunklen);
+    printf("%s\n", chunk);
+    printf("%x\n", opcode);
+    printf("%x\n", opparam);
+    printf("%x\n", nonce);
+
+	// Increment nonce
+    unsigned int new_nonce = nonce + 1;
+
+    // Convert new nonce to network byte order
+    unsigned int new_nonce_network_order = htonl(new_nonce);
+
+    // Build follow-up request
+    unsigned char follow_up_request[4];
+    memcpy(follow_up_request, &new_nonce_network_order, sizeof(new_nonce_network_order));
+
+    // Print follow-up request
+    printf("Follow-up request:\n");
+    print_bytes(follow_up_request, sizeof(follow_up_request));
+
+    // Send follow-up request
+    ssize_t sent_len = sendto(sock, follow_up_request, sizeof(follow_up_request), 0, (struct sockaddr *)&remote_addr, addr_len);
+    if (sent_len == -1) {
+        perror("sendto");
+        return 1;
+    }
+
+    // Receive next directions response
+    response_len = recvfrom(sock, response, sizeof(response), 0, (struct sockaddr *)&remote_addr, &addr_len);
+    if (response_len == -1) {
+        perror("recvfrom");
+        return 1;
+    }
+
+    // Print the size of the response and its contents
+    printf("Received %zd bytes from server\n", response_len);
+    print_bytes(response, response_len);
 
     return 0;
 }
+
 
 void print_bytes(unsigned char *bytes, int byteslen) {
 	int i, j, byteslen_adjusted;
